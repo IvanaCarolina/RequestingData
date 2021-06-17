c9d74b8210ac44efa401ae787bc3f2268e6fd619
Ivana Carolina <75748698+IvanaCarolina@users.noreply.github.com>
2021-06-17 20:24:30 -0300

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -19,14 +19,35 @@ def create_custem_hn(links, votes):
 print(create_custem_hn(links, votes))
 '''
 
+'''
+#adicionar votes 
 def create_custem_hn(links, subtext):
-    hn=[]
+    hn=[] #criar lista
     for inx, item in enumerate(links):
         title = links[inx].getText()
         href = links[inx].get('href', None)
         vote = subtext[inx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace(' points',''))
-            hn.append({'title':title, 'links':href, 'votes':points})
+            if points > 99:
+                hn.append({'title':title, 'links':href, 'votes':points})
     return hn
+pprint.pprint(create_custem_hn(links, subtext))
+'''
+
+#usando sort 
+def sort_stories_by_vote(hnlist):
+    return sorted(hnlist, key = lambda k:k['votes'])
+
+def create_custem_hn(links, subtext):
+    hn=[] #criar lista
+    for inx, item in enumerate(links):
+        title = links[inx].getText()
+        href = links[inx].get('href', None)
+        vote = subtext[inx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points',''))
+            if points > 99:
+                hn.append({'title':title, 'links':href, 'votes':points})
+    return sort_stories_by_vote(hn)
 pprint.pprint(create_custem_hn(links, subtext))
